ba0a53509a7ba74a47c0f2596b579954acfd42a6
Sravya <yourgmail@gmail.com>
2026-02-24 17:31:03 +0530

    Use Render PORT

--- server.js
+++ server.js
@@ -161,6 +161,7 @@ setInterval(load,5000);
 });
 
 // -------- START --------
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
 });
